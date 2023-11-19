1ac480ee2c2d5816e7de9de93a091711913d2e9f
Andreas Bittencourt <andreas.bittencourt@sptech.school>
2023-11-19 14:27:50 -0300

    refactor:Altera lógica

--- app/src/main/java/com/stack/open_work_mobile/services/ProfileService.kt
+++ app/src/main/java/com/stack/open_work_mobile/services/ProfileService.kt
@@ -2,11 +2,19 @@ package com.stack.open_work_mobile.services
 
 import com.stack.open_work_mobile.models.ApiResponse
 import retrofit2.Call
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface ProfileService {
 
     @GET("/api/usuarios/perfil/{userId}")
     fun getProfileInfo(@Path("userId") id: Long): Call<ApiResponse>
+
+    @PUT("/api/usuarios/{userId}")
+    fun putProfileInfo(@Path("userId") id: Long): Call<ApiResponse>
+
+    @DELETE("/api/usuarios/deletar/{userid}")
+    fun deleteProfile(@Path("userId") id: Long): Call<ApiResponse>
 }
