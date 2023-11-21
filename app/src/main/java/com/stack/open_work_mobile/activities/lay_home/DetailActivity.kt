4f2247fc42893c555bfb0ee747fbef01b7c5dc2e
Guilherme Tarifa <guilherme.vieira@sptech.school>
2023-11-21 11:47:02 -0300

    feat: add detail view and kt

---
+++ app/src/main/java/com/stack/open_work_mobile/activities/lay_home/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.stack.open_work_mobile.activities.lay_home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.stack.open_work_mobile.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
